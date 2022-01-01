c457611de566c07e9438a9b19e674a58aaa2ca77
plshazer <pls@hazear.com>
2022-01-01 15:17:37 -0300

    correccion

--- js/index.js
+++ js/index.js
@@ -3,11 +3,36 @@ class Usuario{
  constructor (nombre, apellido, libros, mascotas){
     this.nombre = nombre;
     this.apellido = apellido;
-    this.libros = [];
+    this.libros = [{}];
     this.mascotas = [] ;
- }  
+}  
+getFullName(){
+    console.log(`${user1.nombre} ${user1.apellido}`)
 }
 
+addMascota(nuevaMascota){
+    user1.mascotas.push(nuevaMascota)
+}
+
+countMascotas(){
+    console.log(user1.mascotas.length)
+}
+
+//addBook('Analisis Tecnico de los Mercados Financieros', 'Murphy')
+addBook(titulo, autor){
+user1.libros.push(new libros(titulo, autor))
+}
+
+getBookNames(){
+    let titulos = []
+    for (let index = 0; index < user1.libros.length; index++) {
+        titulos[index] = user1.libros[index].titulo   
+    }
+  
+    console.log(titulos);
+}
+
+}
 class libros{
     constructor(titulo, autor){
         this.titulo = titulo;
@@ -15,31 +40,14 @@ class libros{
     }
 }
 
-const user1 = new Usuario('pedro', 'lopez slevin', 'Analisis Tecnico de los Mercados Financieros','maggie')
-function getFullName(){
-    console.log(`${user1.nombre} ${user1.apellido}`)
-}
+//let librosuser1 = new libros('Analisis Tecnico de los Mercados Financieros', 'Murphy')
+let librosuser1 = {titulo:'Analisis Tecnico de los Mercados Financieros', autor:'Murphy'}
+console.log(librosuser1)
+let user1 = new Usuario('pedro', 'lopez slevin', librosuser1, 'perro');
 
-
-function addMascota(nuevaMascota){
-    user1.mascotas.push(nuevaMascota)
-}
-
-function countMascotas(){
-    console.log(user1.mascotas.length)
-}
-
-//test run :)
-//addBook('Analisis Tecnico de los Mercados Financieros', 'Murphy')
-function addBook(titulo, autor){
-user1.libros.push(new libros(titulo, autor))
-}
-
-function getBookNames(){
-    let titulos = []
-    for (let index = 0; index < user1.libros.length; index++) {
-        titulos[index] = user1.libros[index].titulo   
-    }
-  
-    return titulos;
-}
+//test
+user1.getFullName();
+user1.addMascota('gato');
+user1.countMascotas();
+user1.addBook('Cronicas Marcianas', 'Ray Bradbury');
+user1.getBookNames();
