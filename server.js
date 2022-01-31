2cc041ff4ce8d020841dcef239b1139e271a56aa
plopezslevin <pedro.lopez@mitrol.net>
2022-01-31 04:38:27 -0300

    funcionalidad handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 //----------------------------MODULOS----------------------------
+const fs = require('fs')
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser')
+//handlebars
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //instancia express
 const app = express();
@@ -12,13 +16,59 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json())
 router.use(express.json())
 app.use(express.static('public'))
+app.use(express.static('views'))
+
+//----------------------------PLANTILLAS----------------------------
+//HANDLEBARS
+//CONFIGURACION DE HANDLEBARS
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: 'hbs'
+}))
+//HANDLEBARS:
+app.set('view engine', 'hbs');
+
+//------------------------------------------------------------------
+
+
+//PUG
+//app.set('views', './views')
+//app.set('view engine', 'pug');
+
+
+//FUNCIONALIDAD
+app.engine('html', async (filePath, options, callback)=>{
+    try {
+     const contenido = await fs.promises.readFile(filePath);
+     const htmlGenerado = contenido.toString()
+                                    .replace('$$id$$', options.id)
+                                    .replace('$$title$$', options.title)
+                                    .replace('$$price$$', options.price)
+                                    .replace('$$thumbnail$$', options.thumbnail);
+    
+     callback(null, htmlGenerado)
+    } catch (error) {
+        callback(new Error(error), null)
+    }
+})
+
+//FIN HANDLEBARS
+
+
 
 //----------------------------RUTAS----------------------------
 //PRODUCTOS
 app.use('/api/productos', router);
 let productos = [];
+
 router.get('/', (req, res) => {
-    res.json(productos);
+    if(productos.length===0){res.send('no hay productos')}else{
+        res.json(productos);
+    //res.render('plantilla', productos)
+    }
 });
 
 router.post('/', (req, res) => {
@@ -29,8 +79,10 @@ router.post('/', (req, res) => {
         thumbnail: req.body.thumbnail
     }
     productos.push(producto);
-    console.log(producto)
-    res.json(producto)
+    console.log(producto);
+    //HANDLEBARS
+    res.render('plantilla', producto)
+    
 })
 
 
