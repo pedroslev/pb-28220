0c6d2b299266551d13ab441b6663dadf9dc2738e
plopezslevin <pedro.lopez@mitrol.net>
2022-02-24 17:02:38 -0300

    made sqlite ecommerce db and MariaDB testing

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     let producto = {
-        id: productos.length + 1,
+        //id not needed bc DB got autoincremental
+//        id: productos.length + 1,
         title: req.body.title,
         price: req.body.price,
         thumbnail: req.body.thumbnail
@@ -41,14 +42,60 @@ router.post('/', (req, res) => {
     //productos.push(producto);
     console.log(producto);
     //res.json(producto)
+    //db insertion  
+    mariadb('productos').insert(producto)
+        .then(() => console.log('data inserted successfully'))
+        .catch((err) => {console.log(`Unexpected inserting data on table -> : ${err}`)})
+        .finally(() => knex.destroy())
+
     res.redirect('/')
     //res.status(200)
 })
 
+//----------------------------DB's----------------------------
+//sqlite for mensajes
+const sqlite = require('knex')({
+    client: 'better-sqlite3',
+    connection: {filename: './DB/ecommerce.sqlite'},
+    useNullAsDefault: true
+})
+//table creation of mensajes sqlite3
+//needed only once
+/*
+sqlite.schema.createTable('mensajes', table => {
+        table.increments('id')
+        table.varchar('email'),
+        table.varchar('mensaje')
+    })
+    .then(() => console.log(`table mensajes created`))
+    .catch((err) => {console.log(`Unexpected Creating table Error: ${err}`)})
+  */
+
+//mariaDB(mysql) for productos :)
+const mariadb = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'admin',
+        password: 'addme',
+        database: 'ecom'
+    },
+    pool: {min: 0, max:7}
+})
+/*
+//needed only once (scripted)
+mariadb.schema.createTable('productos', table => {
+    table.increments('id')
+    table.varchar('title'),
+    table.varchar('price'),
+    table.varchar('thumbnail')
+})
+.then(() => console.log(`table productos created`))
+.catch((err) => {console.log(`Unexpected Creating table Error: ${err}`)})
+*/
 
 //----------------------------Sockets----------------------------
 const mensajes = []
-
 io.on('connection', socket => {
     console.log('user connection established successfully')
 
@@ -59,6 +106,11 @@ io.on('connection', socket => {
     socket.on('mensaje', data => {
         mensajes.push({ email: data, mensaje: data })
         io.sockets.emit('mensajes', mensajes)
+        //insertar en db sqlite
+        sqlite('mensajes').insert(data)
+        .then(() => console.log('data inserted successfully'))
+        .catch((err) => {console.log(`Unexpected inserting data on table -> : ${err}`)})
+        .finally(() => knex.destroy())
     })
 
     socket.emit('productos', productos)
@@ -68,8 +120,6 @@ io.on('connection', socket => {
         io.sockets.emit('productos', productos)
     })
 })
-
-
 //----------------------------SERVER----------------------------
 const PORT = 8080;
 httpServer.listen(PORT, () => console.log('Server on!'))
