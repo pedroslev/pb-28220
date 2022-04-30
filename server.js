f55ad534c2d5421370f99eefc8363a9c187f0fc7
pedroslev <plopezslevin@gmail.com>
2022-04-29 22:48:44 -0300

    profiler, lo test y logs

--- server.js
+++ server.js
@@ -1,57 +1,12 @@
 //----------------------------MODULOS----------------------------
-const fs = require('fs')
-const config = require('./config.js')
 const express = require('express');
+const log4js = require('log4js');
 const http = require('http')
 const cluster = require('cluster')
 const numCPUs = require('os').cpus().length
 const info = express.Router();
-const random = express.Router();
 const bodyParser = require('body-parser');
-//instancia express
-const app = express();
-const MongoStore = require('connect-mongo')
-const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
-
-
-//----------------------------Middlewares----------------------------
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))
-const { reset } = require('nodemon');
-
-
-//----------------------------RUTAS----------------------------
-//RANDOM
-app.use('/random', random)
-
-random.post('/', (req,res) => {
-    const cant = req.body.cant
-    let dev = []
-    if(cant = NULL){
-        for (let index = 0; index < 100000000; index++) {
-            dev.push(Math.random(1,1000))
-        }
-    }else{
-        for (let index = 0; index < cant; index++) {
-            dev.push(Math.random(1,1000))
-        }
-    }
-    res.send(cant)
-})
-
-//INFO
-app.use('/info', info)
-
-info.get('/', (req, res) => {
-    res.send(`plataforma: `+process.platform+      
-        `nodejs v:`+ process.version+
-        `memPool: `+process.memoryUsage()+
-        `title: `+process.title+
-        `PID: `+process.pid+
-        `dir: `+process.cwd())
-})
-
-
+const compression = require('compression')
 
 
 //----------------------------CLUSTER----------------------------
@@ -65,11 +20,71 @@ if(cluster.isMaster){
         console.log(`worker ${worker.process.pid} died`)
     })
 }else{
-    http.createServer((req, res) => {
-        res.writeHead(200)
-        res.end('hello world');
-    }).listen(8080)
-    console.log(`worker ${process.pid} started`)
+    const app = express();
+app.use(compression())
+
+log4js.configure({
+    appenders: {
+        LoggerConsole: {type: 'console'},
+        LoggerFileWarning: {type: 'file', filename: './log/warn.log'},
+        LoggerFileError: {type: 'file', filename: './log/error.log'}
+    },
+    categories:{
+        default: {appenders: ["LoggerConsole"], level: "trace"},
+        consola: {appenders: ["LoggerConsole"], level: "debug"},
+        error: {appenders: ["LoggerFileError"], level: "info"},
+        warning: {appenders: ["LoggerFileWarning"], level: "warn"},
+        todos: {appenders: ["LoggerConsole", "LoggerFileWarning", "LoggerFileError"], level: "error"}
+    }
+})
+const loggerInfo = log4js.getLogger('default')
+const loggerWarn = log4js.getLogger('warn')
+const loggerError = log4js.getLogger('err')
+
+//----------------------------Middlewares----------------------------
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+const { reset } = require('nodemon');
+
+
+//----------------------------RUTAS----------------------------
+//INFO
+app.use('/info', info)
+
+info.get('/', (req, res) => {
+    loggerInfo.info(`se ha requerido: `+req)
+    try {
+        res.send(`plataforma: `+process.platform+      
+        `nodejs v:`+ process.version+
+        `memPool: `+process.memoryUsage()+
+        `title: `+process.title+
+        `PID: `+process.pid+
+        `dir: `+process.cwd())    
+    } catch (error) {
+     loggerError.error(error)
+    }
+    
+        
+})
+
+app.use(function(req, res) {
+    loggerWarn.warn(`se ha requerido: `+req)
+          res.json({
+            error: {
+              'name':'Error',
+              'status':404,
+              'message':'Invalid Request',
+              'statusCode':404,
+              'stack':'http://localhost:8080/'
+            },
+             message: 'Testing!'
+          });
+    });
+
+    app.listen('8080', () => {
+        loggerInfo.log('server express levantado en 8080')
+        loggerInfo.log(`PID ${process.pid}`)
+    })
 }
 
 
